80521736745b744c708f586529410137699d7a20
ßingen <bingen@aragon.one>
2020-02-18 23:09:57 +0100

    Update confirm date only when opposed ruling is not zero
    Address PR #41.

--- src/DisputeManager.ts
+++ src/DisputeManager.ts
@@ -183,7 +183,9 @@ function updateAppeal(disputeId: BigInt, roundNumber: BigInt, event: EthereumEve
   appeal.settled = false
   appeal.appealDeposit = nextRound.value6
   appeal.confirmAppealDeposit = nextRound.value7
-  appeal.confirmedAt = event.block.timestamp
+  if (appeal.opposedRuling.gt(new BigInt(0))) {
+    appeal.confirmedAt = event.block.timestamp
+  }
   appeal.save()
 }
 
