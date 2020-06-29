d16e33f8e7e08e8e1608ba3ff85b48800a826a13
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-06-29 19:30:51 -0300

    disputes: fix agreement content mapping

--- helpers/disputable.ts
+++ helpers/disputable.ts
@@ -26,8 +26,8 @@ export function tryDecodingAgreementMetadata(dispute: Dispute): void {
 
     let disputable = new Disputable(buildAgreementActionId(agreement._address, challengeId))
     disputable.dispute = dispute.id
-    disputable.title = settingData.value0
-    disputable.agreement = settingData.value1.toString()
+    disputable.title = settingData.value1
+    disputable.agreement = settingData.value2.toString()
     disputable.actionId = challengeId
     disputable.address = actionData.value0
     disputable.disputableActionId = actionData.value1
