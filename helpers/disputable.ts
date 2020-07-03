3d24d84666b28f93c187e09f5613a2905fc24b1f
ßingen <bingen@aragon.one>
2020-07-03 17:05:45 +0200

    agreement: Simplify dispute metadata construction
    See: https://github.com/aragon/aragon-apps/pull/1188

--- helpers/disputable.ts
+++ helpers/disputable.ts
@@ -4,15 +4,15 @@ import { Disputable, Dispute } from '../types/schema'
 import { crypto, Bytes, Address, BigInt } from '@graphprotocol/graph-ts'
 
 // appId for 'agreement.open.aragonpm.eth'
-const AGREEMENT_DISPUTE_HEADER = '34c62f3aec3073826f39c2c35e9a1297d9dbf3cc77472283106f09eee9cf47bf3a'
-const AGREEMENT_DISPUTE_METADATA_LENGTH = 65 // "[APP_ID]:[CHALLENGE_ID]" = 32 + 1 + 32
+const AGREEMENT_DISPUTE_HEADER = '34c62f3aec3073826f39c2c35e9a1297d9dbf3cc77472283106f09eee9cf47bf'
+const AGREEMENT_DISPUTE_METADATA_LENGTH = 64 // "[APP_ID][CHALLENGE_ID]" = 32 + 32
 
 export function tryDecodingAgreementMetadata(dispute: Dispute): void {
   let metadata = dispute.metadata
   if (metadata.length != AGREEMENT_DISPUTE_METADATA_LENGTH) return
 
   let header = metadata.subarray(0, AGREEMENT_DISPUTE_HEADER.length / 2) as Bytes
-  if (header.toHexString().slice(2) != AGREEMENT_DISPUTE_HEADER) return
+  if (header.toHexString() != AGREEMENT_DISPUTE_HEADER) return
 
   let rawChallengeId = metadata.subarray(AGREEMENT_DISPUTE_HEADER.length / 2, metadata.length) as Bytes
   let challengeId = BigInt.fromSignedBytes(rawChallengeId.reverse() as Bytes)
