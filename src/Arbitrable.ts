835092c5f5d0830e60de031e8c9d4a6847de3071
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-07 18:29:26 -0300

    Arbitrable: Add comments

--- src/Arbitrable.ts
+++ src/Arbitrable.ts
@@ -3,10 +3,15 @@ import { EthereumEvent, Bytes, BigInt, Address } from '@graphprotocol/graph-ts'
 import { EvidenceSubmitted as EvidenceSubmittedWithoutArbitrator, EvidenceSubmitted1 as EvidenceSubmittedWithArbitrator } from '../types/templates/Arbitrable/Arbitrable'
 
 export function handleEvidenceSubmittedWithoutArbitrator(event: EvidenceSubmittedWithoutArbitrator): void {
+  // This handler function works for the EvidenceSubmitted event supported till Aragon Court v1.1.2
+  // Please read the note below about the second EvidenceSubmitted handler function
   handleEvidenceSubmitted(event, event.params.disputeId, event.params.evidence, event.params.submitter)
 }
 
 export function handleEvidenceSubmittedWithArbitrator(event: EvidenceSubmittedWithArbitrator): void {
+  // This handler function works for the new EvidenceSubmitted event introduced in Aragon Court v1.1.3
+  // We need to have a different handler to support the new event signature, this event differs from the
+  // previous one by adding the arbitrator address to the logged info
   handleEvidenceSubmitted(event, event.params.disputeId, event.params.evidence, event.params.submitter)
 }
 
