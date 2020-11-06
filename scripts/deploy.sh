bf303209d10bb45b81eca3061f54b710e0f5424f
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-11-06 13:11:51 -0300

    Update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,28 +28,9 @@ else
   GRAPH_NODE="https://api.thegraph.com/deploy/"
 fi
 
-# Create subgraph if missing
-{
-  graph create aragon/aragon-court${SUBGRAPH_EXT} --node ${GRAPH_NODE}
-} || {
-  echo 'Subgraph was already created'
-}
-
 # Deploy subgraph
-graph deploy aragon/aragon-court${SUBGRAPH_EXT} \
+graph deploy 1hive/celeste${SUBGRAPH_EXT} \
   --ipfs ${IPFS_NODE} \
   --node ${GRAPH_NODE} \
   --access-token "$GRAPHKEY" > deploy-output.txt
 
-SUBGRAPH_ID=$(grep "Build completed:" deploy-output.txt | grep -oE "Qm[a-zA-Z0-9]{44}")
-rm deploy-output.txt
-echo "The Graph deployment complete: ${SUBGRAPH_ID}"
-
-if [[ -z "$SUBGRAPH_ID" ]]; then
-  echo "Could not find subgraph ID in deploy output, cannot deploy to Aragon infra."
-else
-  echo "Deploying subgraph ${SUBGRAPH_ID} to Aragon infra..."
-  kubectl exec graph-shell-0 -- create aragon/aragon-court${SUBGRAPH_EXT}
-  kubectl exec graph-shell-0 -- deploy aragon/aragon-court${SUBGRAPH_EXT} ${SUBGRAPH_ID} graph_index_node_0
-  kubectl exec graph-shell-0 -- reassign aragon/aragon-court${SUBGRAPH_EXT} ${SUBGRAPH_ID} graph_index_node_0
-fi
